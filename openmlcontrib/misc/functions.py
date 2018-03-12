5b69d4dd24f2da84a018e2f41a4f34a56380de6a
janvanrijn@gmail.com <janvanrijn@gmail.com>
2018-03-12 16:30:04 -0400

    added test function

--- openmlcontrib/misc/functions.py
+++ openmlcontrib/misc/functions.py
@@ -1,23 +1,27 @@
 
-def filter_listing(listing, property_name, allowed_values=None):
+def filter_listing(listing, property_name, allowed_values, dict_representation=True):
     """
     Removes items from the result of a listing fn if a property
     does not comply with a given value. 
 
      Parameters
         ----------
-        setupid_setup : dict of dicts, representing openml objects
-            as obtained from an openml listing fn 
+        setupid_setup : dict of dicts or objects, representing 
+            openml objects as obtained from an openml listing fn 
 
         property_name : str
             the name of the property which values should be restricted
 
         allowed_values : list
             list of allowed values
+        
+        dict_representation : bool
+            wether the individual items are represented as dicts 
+            or objects
 
         Returns
         -------
-        model : dict of dicts
+        model : dict of dicts or objects
             a dict, with the objects that did not comply removed 
     """
     allowed = dict()
@@ -25,10 +29,17 @@ def filter_listing(listing, property_name, allowed_values=None):
         raise ValueError('allowed values should be a list')
 
     for id, object in listing.items():
-        if property_name not in object:
-            raise ValueError('dict does not have property: %s' %property_name)
+        if dict_representation:
+            if property_name not in object:
+                raise ValueError('dict does not have property: %s' %property_name)
 
-        if object[property_name] in allowed_values:
-            allowed[id] = object
+            if object[property_name] in allowed_values:
+                allowed[id] = object
+        else:
+            if not hasattr(object, property_name):
+                raise ValueError('dict does not have property: %s' % property_name)
+
+            if getattr(object, property_name) in allowed_values:
+                allowed[id] = object
 
     return allowed
---
+++ tests/test_misc/test_functions.py
@@ -0,0 +1,40 @@
+import openml
+import openmlcontrib
+import unittest
+
+
+class TestMiscFunctions(unittest.TestCase):
+
+    def setUp(self):
+        openml.config.server = "https://test.openml.org/api/v1/xml/"
+
+    def test_filter_data_listing(self):
+        prop = 'name'
+        value = 'kr-vs-kp'
+        data_list = openml.datasets.list_datasets(tag='study_14')
+
+        data_list_prime = openmlcontrib.misc.filter_listing(data_list, prop, [value])
+
+        self.assertEqual(len(data_list_prime), 1)
+        data_id = next(iter(data_list_prime))
+        self.assertEqual(data_list_prime[data_id][prop], value)
+
+    def test_filter_task_listing(self):
+        prop = 'did'
+        value = 1
+        data_list = openml.datasets.list_datasets(tag='study_14')
+
+        task_list_prime = openmlcontrib.misc.filter_listing(data_list, prop, [value])
+
+        self.assertEqual(len(task_list_prime), 1)
+        task_id = next(iter(task_list_prime))
+        self.assertEqual(task_list_prime[task_id][prop], value)
+
+
+    def test_filter_task_listing_illeg(self):
+        prop = 'doesntexist'
+        value = 1
+        data_list = openml.datasets.list_datasets(tag='study_14')
+
+        with self.assertRaises(ValueError):
+            openmlcontrib.misc.filter_listing(data_list, prop, value)
