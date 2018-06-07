07dc207f9a4c063aa0b6c65bd516d870848c2076
janvanrijn <janvanrijn@gmail.com>
2018-06-06 20:04:48 -0400

    setup in config space

--- openmlcontrib/setups/__init__.py
+++ openmlcontrib/setups/__init__.py
@@ -1,1 +1,1 @@
-from .functions import filter_setup_list, obtain_setups_by_ids, setup_to_configuration
+from .functions import filter_setup_list, obtain_setups_by_ids, setup_to_configuration, setup_in_config_space
