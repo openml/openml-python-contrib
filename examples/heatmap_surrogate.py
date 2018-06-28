cc70cfbdaced5aa2ab46e507dba3844b68d8111e
janvanrijn <janvanrijn@gmail.com>
2018-06-28 08:09:32 -0400

    heatmap example

--- examples/heatmap_surrogate.py
+++ examples/heatmap_surrogate.py
@@ -2,11 +2,10 @@ import argparse
 import ConfigSpace
 import matplotlib.pyplot as plt
 import numpy as np
+import openml
 import openmlcontrib.testing
 import os
-import pandas as pd
 import sklearn.ensemble
-
 
 
 def parse_args():
@@ -14,11 +13,13 @@ def parse_args():
     parser.add_argument('--cache_directory', type=str, default=os.path.expanduser('~') + '/experiments/openml_cache')
     parser.add_argument('--task_id', type=int, default=3)
     parser.add_argument('--n_estimators', type=int, default=64)
-    parser.add_argument('--num_runs', type=int, default=1500)
+    parser.add_argument('--num_runs', type=int, default=500)
     parser.add_argument('--evaluation_measure', type=str, default='predictive_accuracy')
     parser.add_argument('--x_param', type=str, default='C')
     parser.add_argument('--y_param', type=str, default='gamma')
-    parser.add_argument('--resolution', type=int, default=16)
+    parser.add_argument('--resolution', type=int, default=32)
+    parser.add_argument('--n_contour_lines', type=int, default=16)
+    parser.add_argument('--output_directory', type=str, default=os.path.expanduser('~') + '/experiments/openml')
 
     return parser.parse_args()
 
@@ -36,17 +37,22 @@ def get_flow_and_config_space():
     return flow_id, cs
 
 
-def get_param_indices(param, resolution):
+def get_param_indices(param, resolution, tol=0.00001):
     if param.log:
-        return np.logspace(np.log(param.lower), np.log(param.upper), resolution)
+        space = np.logspace(np.log10(param.lower), np.log10(param.upper), resolution, base=10)
     else:
-        return np.linspace(param.lower, param.upper, resolution)
+        space = np.linspace(param.lower, param.upper, resolution)
+    if np.abs(space[0] - param.lower) > tol:
+        raise ValueError('Space does not start with param lower bound %f: %f' % (param.lower, space[0]))
+    if np.abs(space[-1] - param.upper) > tol:
+        raise ValueError('Space does not end with param upper bound %f: %f' % (param.upper, space[-1]))
+    return space
 
 
 def run():
     args = parse_args()
     flow_id, configuration_space = get_flow_and_config_space()
-
+    dataset_name = openml.tasks.get_task(args.task_id).get_dataset().name
     df = openmlcontrib.meta.get_task_flow_results_as_dataframe(args.task_id, flow_id, args.num_runs,
                                                                configuration_space, 'parameter_name',
                                                                args.evaluation_measure, args.cache_directory)
@@ -62,24 +68,36 @@ def run():
 
     x_param = configuration_space.get_hyperparameter(args.x_param)
     y_param = configuration_space.get_hyperparameter(args.y_param)
+    x_resolution = args.resolution
+    y_resolution = args.resolution
+    XX, YY = np.meshgrid(get_param_indices(x_param, x_resolution), get_param_indices(y_param, y_resolution))
+    ZZ = np.zeros((y_resolution, x_resolution), dtype=np.float64)
 
-    XX, YY = np.meshgrid(get_param_indices(x_param, args.resolution), get_param_indices(y_param, args.resolution))
-    ZZ = np.zeros((len(YY), len(XX)), dtype=np.float64)
-    for i in range(args.resolution):
-        for j in range(args.resolution):
-            data_point = [[YY[i, j], XX[i, j]]]
+    for i in range(y_resolution):
+        for j in range(x_resolution):
+            data_point = [[XX[i, j], YY[i, j]]]
             # TODO check indices (x / y mixup)
             ZZ[i, j] = estimator.predict(data_point)[0]
 
     fig, axes = plt.subplots(1, 1)
-    axes.contour(XX, YY, ZZ)
+    contours = axes.contourf(XX, YY, ZZ, args.n_contour_lines, cmap=plt.cm.viridis)
+    fig.colorbar(contours)
+
     axes.set_xlim(x_param.lower, x_param.upper)
     axes.set_ylim(y_param.lower, y_param.upper)
     if x_param.log:
         axes.set_xscale("log")
     if y_param.log:
         axes.set_yscale("log")
-    plt.show()
+
+    axes.set_title('Contour plot on Task %d: %s' % (args.task_id, dataset_name))
+    axes.set_xlabel(args.x_param)
+    axes.set_ylabel(args.y_param)
+    if args.output_directory:
+        os.makedirs(args.output_directory, exist_ok=True)
+        plt.savefig(os.path.join(args.output_directory, 'contourplot-task-%d.png' % args.task_id))
+    else:
+        plt.show()
 
 
 if __name__ == '__main__':
