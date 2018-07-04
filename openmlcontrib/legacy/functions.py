676e6781bd3ec63b9074ef04b0bde6d80617f229
janvanrijn <janvanrijn@gmail.com>
2018-07-04 13:05:36 -0400

    added documentationd and made fn private

--- openmlcontrib/legacy/functions.py
+++ openmlcontrib/legacy/functions.py
@@ -3,7 +3,21 @@ import numpy as np
 import typing
 
 
-def interpret_string(hyperparameter: ConfigSpace.hyperparameters.Hyperparameter):
+def _interpret_string(hyperparameter: ConfigSpace.hyperparameters.Hyperparameter) -> bool:
+    """
+    Whether the ConfigSpace hyperparameter values should be interpreted in string format. This happens for categorical
+    features (even when the values are all numeric) and sometimes for UnParameterized or Constants.
+
+    Parameters
+    ----------
+    hyperparameter: ConfigSpace.hyperparameters.Hyperparameter
+        The hyperparameter to inspect
+
+    Returns
+    ----------
+    bool
+        Whether the hyperparameter values should be interpreted as strings
+    """
     if isinstance(hyperparameter, ConfigSpace.CategoricalHyperparameter):
         return True
     if isinstance(hyperparameter, ConfigSpace.UnParametrizedHyperparameter):
@@ -34,7 +48,7 @@ def get_active_hyperparameters(configuration_space: ConfigSpace.ConfigurationSpa
     for hyperparameter in configuration_space.get_hyperparameters():
         name = hyperparameter.name
         # note that the openml-python json sometimes accidentally mistakes strings for bools or numeric values
-        value_to_search = str(key_values[name]) if interpret_string(hyperparameter) else key_values[name]
+        value_to_search = str(key_values[name]) if _interpret_string(hyperparameter) else key_values[name]
         value = hyperparameter._inverse_transform(value_to_search)
         vector[configuration_space._hyperparameter_idx[name]] = value
     active_parameters = set()
