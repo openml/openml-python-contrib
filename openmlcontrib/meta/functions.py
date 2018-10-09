915a1015c9ad567ee3b8134d34e117c60835655c
janvanrijn <janvanrijn@gmail.com>
2018-10-09 19:09:26 -0400

    updated meta function to be able to take into account multiple evaluation measures

--- openmlcontrib/meta/functions.py
+++ openmlcontrib/meta/functions.py
@@ -7,13 +7,58 @@ import pandas as pd
 import pickle
 import os
 import typing
+import warnings
 
 
-def get_task_flow_results_as_dataframe(task_id: int, flow_id: int, num_runs: int, raise_few_runs: bool,
-                                       configuration_space: ConfigSpace.ConfigurationSpace, parameter_field: str,
-                                       evaluation_measure: str, cache_directory: typing.Union[str, None]):
+def _merge_setup_dict_and_evaluation_dicts(
+        setups: typing.Dict[int, openml.setups.OpenMLSetup],
+        flow: openml.flows.OpenMLFlow,
+        configuration_space: ConfigSpace.ConfigurationSpace,
+        evaluations: typing.Dict[str, typing.Dict[int, openml.evaluations.OpenMLEvaluation]]) \
+        -> typing.Dict[int, typing.Dict]:
+    # returns a dict, mapping from setup id to a dict containing all
+    # hyperparameters and evaluation measures
+    setup_evaluations = dict()
+    for measure in evaluations:
+        # evaluations[measure] is a dict mapping from run id to evaluation
+        # we can assume that all results are on the same task, so setup is the determining key
+        # we will reindex this setup_evaluations[measure] to map from a setup id to evaluation measure
+        setup_keys = [eval.setup_id for eval in evaluations[measure].values()]
+        task_keys = [eval.task_id for eval in evaluations[measure].values()]
+        if len(set(task_keys)) != 1:
+            # this should never happen
+            raise KeyError('Found multiple task keys in the result set for measure %s' % measure)
+        if set(setup_keys) != set(setups.keys()):
+            # this should also never happen, and hints at either a bug in setup
+            # listing or evaluation listing not complete
+            raise KeyError('Setup keys do not align for measure %s' % measure)
+        setup_evaluations[measure] = {eval.setup_id: eval for eval in evaluations[measure].values()}
+        if len(setup_evaluations[measure]) != len(evaluations[measure]):
+            raise KeyError('Lengths of reindexed dict does not comply with old length. ')
+
+    result = dict()
+    for setup in setups.values():
+        if setup.flow_id != flow.flow_id:
+            # this should never happen
+            raise ValueError('Setup and flow do not align.')
+        setup_dict = openmlcontrib.setups.setup_to_parameter_dict(setup=setup,
+                                                                  flow=flow,
+                                                                  map_library_names=True,
+                                                                  configuration_space=configuration_space)
+        for measure in evaluations:
+            setup_dict[measure] = setup_evaluations[measure][setup.setup_id].value
+        result[setup.setup_id] = setup_dict
+    return result
+
+
+def get_task_flow_results_as_dataframe(task_id: int, flow_id: int,
+                                       num_runs: int, raise_few_runs: bool,
+                                       configuration_space: ConfigSpace.ConfigurationSpace,
+                                       evaluation_measures: typing.List[str],
+                                       cache_directory: typing.Union[str, None]) -> pd.DataFrame:
     """
-    Obtains a number of runs from a given flow on a given task, and returns a (relevant) set of parameters
+    Obtains a number of runs from a given flow on a given task, and returns a
+    (relevant) set of parameters
 
     Parameters
     ----------
@@ -24,55 +69,70 @@ def get_task_flow_results_as_dataframe(task_id: int, flow_id: int, num_runs: int
     num_runs: int
         Maximum on the number of runs per task
     configuration_space: ConfigurationSpace
-        Determines valid parameters and ranges. These will be returned as column names
-    parameter_field: str
-        the key field in the parameter object that should be selected. Use full_name in order to avoid collisions; a
-        good alternative is the use of parameter_name
-    evaluation_measure:
-        Evaluation measure to obtain
-    cache_directory: str or None
+        Determines valid parameters and ranges. These will be returned as
+        column names
+    evaluation_measures: List[str]
+        A list with the evaluation measure to obtain
+    cache_directory: optional, str
         Directory where cache files can be stored to or obtained from
     raise_few_runs: bool
-        Raises an error if not enough runs are found according to the `num_runs` argument
+        Raises an error if not enough runs are found according to the
+        `num_runs` argument
 
     Returns
     -------
-    dataframe : a pandas dataframe
+    df : pd.DataFrame
+        a dataframe with as columns the union of the config_space
+        hyperparameters and the evaluation measures, and num_runs rows.
     """
+    for measure in evaluation_measures:
+        if measure in configuration_space.get_hyperparameters():
+            raise ValueError('measure shadows name in hyperparameter list: %s' % measure)
+    # both cache paths will be set to a value if cache_directory is not None
+    evaluations_cache_path = dict()
+    setups_cache_path = None
 
-    if 'y' in configuration_space.get_hyperparameters():
-        raise ValueError('y is a column name for the evaluation measure')
-
+    # decides the files where the cache will be stored
     if cache_directory is not None:
         cache_flow_task = os.path.join(cache_directory, str(flow_id), str(task_id))
         os.makedirs(cache_flow_task, exist_ok=True)
 
-        evaluations_cache_path = os.path.join(cache_flow_task, 'evaluations_%s_%d.pkl' % (evaluation_measure, num_runs))
+        for measure in evaluation_measures:
+            evaluations_cache_path[measure] = os.path.join(cache_flow_task,
+                                                           'evaluations_%s_%d.pkl' % (measure, num_runs))
         setups_cache_path = os.path.join(cache_flow_task, 'setups_%d.pkl' % num_runs)
 
-    if cache_directory is None or not os.path.isfile(evaluations_cache_path):
-        # downloads (and caches, if allowed) num_runs random evaluations
-        evaluations = openml.evaluations.list_evaluations(evaluation_measure,
-                                                          size=num_runs, task=[task_id], flow=[flow_id])
-        if len(evaluations) < num_runs and raise_few_runs:
-            raise ValueError('Not enough evaluations. Required: %d, Got: %d' % (num_runs, len(evaluations)))
-        if cache_directory is not None and len(evaluations) == num_runs:
-            # important to only store cache if enough runs were obtained
-            with open(evaluations_cache_path, 'wb') as fp:
-                pickle.dump(evaluations, fp)
-    else:
-        # obtains the evaluations from cache
-        with open(evaluations_cache_path, 'rb') as fp:
-            evaluations = pickle.load(fp)
-    if len(evaluations) == 0:
-        raise ValueError('No results on Task %d according to these criteria' % task_id)
+    # downloads (and caches, if allowed) the evaluations for all measures.
+    evaluations = dict()
+    setup_ids = set()  # list maintaining all used setup ids
+    for measure in evaluation_measures:
+        if cache_directory is None or not os.path.isfile(evaluations_cache_path[measure]):
+            # downloads (and caches, if allowed) num_runs random evaluations
+            evals_current_measure = openml.evaluations.list_evaluations(measure,
+                                                                        size=num_runs,
+                                                                        task=[task_id],
+                                                                        flow=[flow_id])
+            if len(evals_current_measure) < num_runs and raise_few_runs:
+                raise ValueError('Not enough evaluations for measure: %s. '
+                                 'Required: %d, Got: %d' % (measure, num_runs,
+                                                            len(evals_current_measure)))
+            if cache_directory is not None and len(evals_current_measure) == num_runs:
+                # important to only store cache if enough runs were obtained
+                with open(evaluations_cache_path[measure], 'wb') as fp:
+                    pickle.dump(evals_current_measure, fp)
+            evaluations[measure] = evals_current_measure
+        else:
+            # obtains the evaluations from cache
+            with open(evaluations_cache_path[measure], 'rb') as fp:
+                evaluations[measure] = pickle.load(fp)
+        if len(evaluations[measure]) == 0:
+            raise ValueError('No results on Task %d measure %s according to these criteria' % (task_id, measure))
+        for eval in evaluations[measure].values():
+            setup_ids.add(eval.setup_id)
 
+    # downloads (and caches, if allowed) the setups that belong to the evaluations
     if cache_directory is None or not os.path.isfile(setups_cache_path):
-        # downloads (and caches, if allowed) the setups that belong to the evaluations
-        setup_ids = []
-        for run_id, evaluation in evaluations.items():
-            setup_ids.append(evaluation.setup_id)
-        setups = openmlcontrib.setups.obtain_setups_by_ids(setup_ids=setup_ids)
+        setups = openmlcontrib.setups.obtain_setups_by_ids(setup_ids=list(setup_ids))
 
         if cache_directory is not None and len(evaluations) == num_runs:
             # important to only store cache if enough runs were obtained
@@ -83,48 +143,57 @@ def get_task_flow_results_as_dataframe(task_id: int, flow_id: int, num_runs: int
         with open(setups_cache_path, 'rb') as fp:
             setups = pickle.load(fp)
 
-    # download flows
+    # download flows. Note that only one flow is allowed, per definition
     flows = dict()
     for setup in setups.values():
         if flow_id not in flows:
             flows[setup.flow_id] = openml.flows.get_flow(setup.flow_id)
     if len(flows) != 1:
+        # This should never happen.
         raise ValueError('Expected exactly one flow. Got %d' % len(flows))
 
+    # initiates the dataframe object
     relevant_parameters = configuration_space.get_hyperparameter_names()
     all_columns = list(relevant_parameters)
-    all_columns.append('y')
-    dataframe = pd.DataFrame(columns=all_columns)
+    for measure in evaluation_measures:
+        all_columns.append(measure)
+    df = pd.DataFrame(columns=all_columns)
 
-    for run_id, evaluation in evaluations.items():
-        current_setup = setups[evaluation.setup_id]
-        if openmlcontrib.setups.setup_in_config_space(current_setup, flows[current_setup.flow_id], configuration_space):
-            current_setup_as_dict = openmlcontrib.setups.setup_to_parameter_dict(setup=current_setup,
-                                                                                 flow=flows[current_setup.flow_id],
-                                                                                 map_library_names=True,
-                                                                                 configuration_space=configuration_space)
-            current_setup_as_dict['y'] = evaluation.value
-            dataframe = dataframe.append(current_setup_as_dict, ignore_index=True)
+    # initiates all records. Note that we need to check them one by one before
+    # we can add them to the dataframe
+    setups_merged = _merge_setup_dict_and_evaluation_dicts(setups,
+                                                           flows[flow_id],
+                                                           configuration_space,
+                                                           evaluations)
+    # adds the applicable setups to the dataframe
+    for setup_id, setup_merged in setups_merged.items():
+        # the setups dict still contains the setup objects
+        current_setup = setups[setup_id]
+        if openmlcontrib.setups.setup_in_config_space(current_setup,
+                                                      flows[current_setup.flow_id],
+                                                      configuration_space):
+            df = df.append(setup_merged, ignore_index=True)
         else:
-            # sometimes, a numeric param can contain string values.
-            # TODO: determine what to do with these. Raise Value, add or skip
-            print('skipping setup', evaluation.setup_id)
+            warnings.warn('Setup does not comply to configuration space: %s ' % setup_id)
 
-    all_numeric_columns = list(['y'])
+    all_numeric_columns = list(evaluation_measures)
     for param in configuration_space.get_hyperparameters():
         if isinstance(param, ConfigSpace.hyperparameters.NumericalHyperparameter):
             all_numeric_columns.append(param.name)
 
-    dataframe[all_numeric_columns] = dataframe[all_numeric_columns].apply(pd.to_numeric)
+    df[all_numeric_columns] = df[all_numeric_columns].apply(pd.to_numeric)
 
-    if dataframe.shape[0] > num_runs:
-        raise ValueError()
-    if dataframe.shape[1] != len(relevant_parameters) + 1:  # plus 1 for y data
-        raise ValueError()
+    if df.shape[0] > num_runs:
+        # this should never happen
+        raise ValueError('Too many runs. Expected %d got %d' % (num_runs, df.shape[0]))
+    exp_params = len(relevant_parameters) + len(evaluation_measures)
+    if df.shape[1] != exp_params:
+        # this should never happen
+        raise ValueError('Wrong number of attributes. Expected %d got %d' % (exp_params, df.shape[1]))
 
-    dataframe = dataframe.reindex(sorted(dataframe.columns), axis=1)
+    df = df.reindex(sorted(df.columns), axis=1)
 
-    return dataframe
+    return df
 
 
 def dataframe_to_arff(dataframe, relation, description):
@@ -151,6 +220,11 @@ def arff_to_dataframe(liacarff):
     if liacarff.keys() != expected_keys:
         raise ValueError('liacarff object does not contain correct keys.')
     data_ = np.array(liacarff['data'])
-    arff_dict = {col_name: pd.Series(data_[:, idx], dtype=np.float64 if str(col_type).lower() in num_keys else object)
-                 for idx, (col_name, col_type) in enumerate(liacarff['attributes'])}
+    arff_dict = {
+        col_name: pd.Series(data_[:, idx],
+                            dtype=np.float64
+                            if str(col_type).lower() in num_keys
+                            else np.dtype(object))
+        for idx, (col_name, col_type) in enumerate(liacarff['attributes'])
+    }
     return pd.DataFrame(arff_dict)
--- tests/test_meta/test_meta_functions.py
+++ tests/test_meta/test_meta_functions.py
@@ -9,16 +9,15 @@ from openmlcontrib.testing import TestBase
 
 class TestMetaFunctions(TestBase):
 
-    def get_task_flow_results_as_dataframe(self, task_id, flow_id, num_configs, config_space):
+    def get_task_flow_results_as_dataframe(self, task_id, flow_id, num_configs, measures, config_space):
         df = openmlcontrib.meta.get_task_flow_results_as_dataframe(task_id, flow_id, num_configs,
                                                                    raise_few_runs=True,
                                                                    configuration_space=config_space,
-                                                                   parameter_field='parameter_name',
-                                                                   evaluation_measure='predictive_accuracy',
+                                                                   evaluation_measures=measures,
                                                                    cache_directory=None)
-
+        n_columns = len(config_space.get_hyperparameter_names()) + len(measures)
         self.assertEqual(type(df), pd.DataFrame)
-        self.assertEqual(df.shape, (num_configs, len(config_space.get_hyperparameter_names()) + 1))
+        self.assertEqual(df.shape, (num_configs, n_columns))
 
         for param in config_space.get_hyperparameters():
             if isinstance(param, ConfigSpace.hyperparameters.NumericalHyperparameter):
@@ -33,9 +32,23 @@ class TestMetaFunctions(TestBase):
                 self.assertIn(df[param.name].dtype, [np.float64, np.int64, object])
             else:
                 raise ValueError()
-        self.assertGreater(df['y'].min(), -1000000)
-        self.assertLessEqual(df['y'].max(), 1000000)
-        self.assertIn(df['y'].dtype, [np.float64, np.int64])
+
+        for measure in measures:
+            # please only test on measures that comply to these numbers
+            self.assertGreaterEqual(df[measure].min(), 0)
+            self.assertLessEqual(df[measure].max(), 1)
+            self.assertIn(df[measure].dtype, [np.float64, np.int64])
+
+        # note that we test the to and from dataframe features in this function
+        arff = openmlcontrib.meta.dataframe_to_arff(df, 'name', None)
+        for param in config_space.get_hyperparameters():
+            # note that arff does not maintain knowledge about integer vs float.
+            # therefore, we transform integer columns to floats
+            if isinstance(param, ConfigSpace.hyperparameters.UniformIntegerHyperparameter):
+                df[param.name] = df[param.name].astype(np.float64)
+
+        df_reproduced = openmlcontrib.meta.arff_to_dataframe(arff)
+        pd.testing.assert_frame_equal(df, df_reproduced)
 
     def test_get_task_flow_results_as_dataframe_svm(self):
         openml.config.server = 'https://www.openml.org/api/v1/'
@@ -45,8 +58,9 @@ class TestMetaFunctions(TestBase):
         flow_id = 7707
         num_configs = 50
         cs = TestBase._get_libsvm_svc_config_space()
+        measures = ['predictive_accuracy']
 
-        self.get_task_flow_results_as_dataframe(task_id, flow_id, num_configs, cs)
+        self.get_task_flow_results_as_dataframe(task_id, flow_id, num_configs, measures, cs)
 
     def test_get_task_flow_results_as_dataframe_rf(self):
         openml.config.server = 'https://www.openml.org/api/v1/'
@@ -56,11 +70,6 @@ class TestMetaFunctions(TestBase):
         flow_id = 6969
         num_configs = 50
         cs = TestBase._get_random_forest_default_search_space()
+        measures = ['predictive_accuracy', 'f_measure', 'area_under_roc_curve']
 
-        self.get_task_flow_results_as_dataframe(task_id, flow_id, num_configs, cs)
-
-    def test_dataframe_to_arff(self):
-        raise NotImplementedError()
-
-    def test_arff_to_dataframe(self):
-        raise NotImplementedError()
+        self.get_task_flow_results_as_dataframe(task_id, flow_id, num_configs, measures, cs)
