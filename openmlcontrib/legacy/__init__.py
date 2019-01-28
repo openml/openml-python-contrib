26974aad188537771e8baf9d5d3ab55df4fd4c06
janvanrijn <janvanrijn@gmail.com>
2019-01-28 17:44:00 -0500

    same as prev

--- openmlcontrib/legacy/__init__.py
+++ openmlcontrib/legacy/__init__.py
@@ -1,1 +1,2 @@
-from .functions import is_integer_hyperparameter, is_boolean_hyperparameter
+from .functions import is_boolean_hyperparameter, is_float_hyperparameter, \
+    is_integer_hyperparameter, is_string_hyperparameter, get_hyperparameter_datatype
