478eadd4c887972bdbecca0c31f6c16e114d8ff0
janvanrijn <janvanrijn@gmail.com>
2019-02-06 14:15:57 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -53,9 +53,5 @@ setuptools.setup(name="openmlcontrib",
                               'Operating System :: POSIX',
                               'Operating System :: Unix',
                               'Operating System :: MacOS',
-                              'Programming Language :: Python :: 2',
-                              'Programming Language :: Python :: 2.7',
-                              'Programming Language :: Python :: 3',
-                              'Programming Language :: Python :: 3.4',
-                              'Programming Language :: Python :: 3.5',
-                              'Programming Language :: Python :: 3.6'])
+                              'Programming Language :: Python :: 3.6',
+                              'Programming Language :: Python :: 3.7'])
